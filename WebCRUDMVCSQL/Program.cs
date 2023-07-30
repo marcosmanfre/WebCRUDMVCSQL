8d19fbb938e28fd50c0428bbe69c62eb9a98dc04
marcosmanfre <marcosmanfre@hotmail.com>
2023-07-30 15:15:11 -0300

    program

--- WebCRUDMVCSQL/Program.cs
+++ WebCRUDMVCSQL/Program.cs
@@ -6,8 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<Contexto>
-    (options => options .UseSqlServer(""))
+
 
 var app = builder.Build();
 
